2d124bee6bcec26aa1022133627f358d6055f32a
Nimesh Madusanka <42901200+NimeshMadusanka@users.noreply.github.com>
2021-06-29 19:56:31 +0530

    add changes

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ def predict():
 
     output = prediction[0]
 
-    return render_template('index.html', prediction_text='Employee Salary should be $ {}'.format(output))
+    return render_template('index.html', prediction_text='Farmer Recommendation is $ {}'.format(output))
 
 @app.route('/predict_api',methods=['POST'])
 def predict_api():
